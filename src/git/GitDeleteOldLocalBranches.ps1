6482febc3e6b6a6223f650f5ce0f0c47f44b90a3
Dale Hirt <dalehhirt@users.noreply.github.com>
2019-05-07 11:11:27 -0700

    Git utility scripts. (#1)

---
+++ src/git/GitDeleteOldLocalBranches.ps1
@@ -0,0 +1,28 @@
+﻿# Get all latest info
+git pull
+git push
+
+# Get branch names
+$branchesToRemove = git branch -vv | where {$_ -match '\[origin/.*: gone\]'} | foreach {$_.split(" ", [StringSplitOptions]'RemoveEmptyEntries')[0]}
+
+if($branchesToRemove -ne $null) {
+    write-host "Branches to delete"
+    write-host "=================="
+    $branchesToRemove
+
+    $message  = 'Delete Branches'
+    $question = 'Are you sure you want to proceed?'
+
+    $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+    $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes'))
+    $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+
+    $decision = $Host.UI.PromptForChoice($message, $question, $choices, 1)
+    if ($decision -eq 0) {
+        $branchesToRemove | foreach {write-host "Deleting " $_; git branch -D $_}
+    }
+}
+else {
+    write-host "No branches to delete."
+}
+write-host "Done"
---
+++ src/git/gitChangeAuthor.ps1
@@ -0,0 +1,28 @@
+﻿param(
+$repoName,
+$repoUrl,
+$oldemail,
+$newemail,
+[switch]$SkipInit)
+
+if(!$SkipInit) {
+    git clone --bare $repoUrl
+    cd "$repoName.git"
+}
+
+$env_filter = '
+OLD_EMAIL="{0}"
+CORRECT_EMAIL="{1}"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' -f $oldemail, $newemail
+
+git filter-branch --env-filter $env_filter
+
+git log --format="%ae" --all | sort -u
