6d7159069f379ab2e9e03b9b446f310f00030437
Dale Hirt <dalehhirt@thehirts.net>
2019-05-08 14:43:22 -0700

    Dev/dalehirt/git scripts (#2)
    * Git utility scripts.
    
    * More scripts from old repo.
    
    * Initial version of every-directory script.

---
+++ src/every-directory.ps1
@@ -0,0 +1,50 @@
+﻿param(  [parameter(Mandatory=$true, ParameterSetName = "ScriptBlock")]
+        [scriptblock]
+        $ScriptBlock,
+
+        [parameter(Mandatory=$true, ParameterSetName = "ScriptFile")]
+        [string] 
+        $ScriptFile, 
+        
+        [switch]
+        $TestFirstOnly)
+
+switch ($PsCmdlet.ParameterSetName) 
+
+{ 
+
+    "ScriptBlock" {
+        if(![System.String]::IsNullOrWhiteSpace($ScriptBlock)) {
+            $currentDirectories = dir | where {$_.PSIsContainer}
+            if($TestFirstOnly) { $currentDirectories = $currentDirectories | Select-Object -First 1 }
+
+            $currentDirectories | ForEach-Object {
+                Push-Location $_
+                write-host -----------
+                write-host $_.FullName
+                write-host -----------
+                &$ScriptBlock
+                Pop-Location
+            } 
+        }
+        break
+    }
+
+    "ScriptFile"  {
+        if(Test-Path $ScriptFile) {
+            $currentDirectories = dir | where {$_.PSIsContainer}
+            if($TestFirstOnly) { $currentDirectories = $currentDirectories | Select-Object -First 1 }
+
+            $currentDirectories | ForEach-Object {
+                Push-Location $_
+                write-host -----------
+                write-host $_.FullName
+                write-host -----------
+                &$ScriptFile
+                Pop-Location
+            } 
+        }
+        break
+    }
+} 
+
---
+++ src/git/SyncGithubRepos.ps1
@@ -0,0 +1,25 @@
+﻿param($DefaultEmailAddress)
+
+# Go up two directories to base Github directory
+Push-Location
+Set-Location (get-item $PSScriptRoot).parent.parent.FullName
+
+function Sync-Repository() {
+    param($Repository)
+    if (!(Test-Path $Repository.name)) {
+        write-host Creating $Repository.name
+        git clone $Repository.clone_url
+    }
+
+    Push-Location
+    Set-Location $Repository.name
+
+    git config user.email $DefaultEmailAddress
+
+    Pop-Location
+}
+
+$repos = Get-Content (Join-Path $PSScriptRoot Repository.txt) | ConvertFrom-Json
+$repos | where {$_.name -ne "PublicDale"} | ForEach-Object {Sync-Repository $_}
+
+Pop-Location
