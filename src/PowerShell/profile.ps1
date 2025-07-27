4c9f04d8a37fb521d4a3e47de98438b6bfd12f9f
Dale Hirt <2246127+dalehhirt@users.noreply.github.com>
2025-07-26 23:31:21 -0700

    feat: basic profile.ps1

---
+++ src/PowerShell/profile.ps1
@@ -0,0 +1,35 @@
+function Using-Object() {
+    <#
+    .DESCRIPTION
+    Allows for disposal of objects
+    .EXAMPLE
+    Using-Object ($sw = [System.IO.StreamWriter]::new("some file.txt")) {
+      $sw.WriteLine('some text')
+    }
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [AllowEmptyString()]
+        [AllowEmptyCollection()]
+        [AllowNull()]
+        [Object]
+        $InputObject,
+        [Parameter(Mandatory = $true)]
+        [scriptblock]
+        $ScriptBlock
+    )
+
+    try {
+        . $ScriptBlock
+    }
+    finally {
+        if ($null -ne $InputObject -and $InputObject -is [System.IDisposable]) {
+            $InputObject.Dispose()
+        }
+    }
+}
+
+function log() {
+  write-host ">>> [$($env:COMPUTERNAME)]" ((Get-Date).ToUniversalTime().ToString('u')) "$args" -ForeGroundColor Yellow
+}
