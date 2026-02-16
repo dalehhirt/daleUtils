726a92fa363d7b1e77f4fac6297b9a73620af8fb
Dale Hirt <2246127+dalehhirt@users.noreply.github.com>
2026-02-16 09:17:50 -0500

    fix: updating to take subscription objects

--- src/Azure/ElevateMe.ps1
+++ src/Azure/ElevateMe.ps1
@@ -7,12 +7,27 @@ C:\personal\daleUtils\src\Azure\ElevateMe.ps1 -subscriptionId "<subscription id>
 C:\personal\daleUtils\src\Azure\ElevateMe.ps1 -Justification "Terraform Deployment" -whatif
 .EXAMPLE
 Get-AzSubscription | foreach {C:\personal\daleUtils\src\Azure\ElevateMe.ps1 -subscriptionId $_.SubscriptionId -Justification "Terraform Deployment" -whatif}
+.EXAMPLE
+Get-Azsubscription| sort Name | C:\personal\daleUtils\src\Azure\ElevateMe.ps1 -Justification "Terraform Deployment" 
 #>
-[CmdletBinding(SupportsShouldProcess=$true)]
+[CmdletBinding(SupportsShouldProcess = $true)]
 param(
-  $subscriptionId = '',
+  [Parameter(Mandatory, ParameterSetName = 'subscriptions', ValueFromPipeline)]
+  [object[]]$subscriptions,
+
+  [Parameter(Mandatory, ParameterSetName = 'subscriptionId')]
+  [string]$subscriptionId,
+
+  [Parameter(ParameterSetName = 'subscriptions')]
+  [Parameter(ParameterSetName = 'subscriptionId')]
   $Justification,
+
+  [Parameter(ParameterSetName = 'subscriptions')]
+  [Parameter(ParameterSetName = 'subscriptionId')]
   $Hours = 8,
+
+  [Parameter(ParameterSetName = 'subscriptions')]
+  [Parameter(ParameterSetName = 'subscriptionId')]
   [ValidateNotNullOrEmpty()][DateTime]$NotBefore = [DateTime]::Now,
   $rolesToIgnore = @("Reader")
 )
@@ -23,75 +38,75 @@ begin {
   
   function log-error() {
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs', '',
-      Justification='Log error is more accurate')]
+      Justification = 'Log error is more accurate')]
     param()
   
     Write-Error ">>> [$($env:COMPUTERNAME)] $((Get-Date).ToUniversalTime().ToString('u')) $args"
   }
-  
-  $filter = 'asTarget()'
-  $Scope = '/'
-  $isSubIdNull = [System.String]::IsNullOrWhiteSpace($subscriptionId)
 
-  # We don't actually do anything unless subscription is specified
-  if ($isSubIdNull -and ($WhatIfPreference -eq $false)) {
-    log "Setting Whatif to true. Pass in subscriptionId to enable."
-    $WhatIfPreference = $true
-  }
-}
-process {
-  if ($isSubIdNull) {
-    log "Getting role information for all subscription(s)."
-  }
-  else {
-    log "Getting role information for subscription $subscriptionId"
-  }
-  $eligibleRoles = Get-AzRoleEligibilitySchedule -Scope $scope -Filter $filter -ErrorAction stop |
-    Sort-Object Id
-
-  $activatedRoles = Get-AzRoleAssignmentScheduleInstance -Scope $scope -Filter $filter -ErrorAction stop |
-          Where-Object {$_.AssignmentType -EQ 'Activated' } |
-          Sort-Object Id
-
-  if (!$isSubIdNull) {
-    $eligibleRoles = $eligibleRoles | Where-Object {$_.Id -like "/subscriptions/$subscriptionId*"}
-    $activatedRoles = $activatedRoles | Where-Object {$_.Id -like "/subscriptions/$subscriptionId*"}
-  }
-
-  $activatedRolesIds = $activatedRoles.LinkedRoleEligibilityScheduleId
-
-  if($null -ne $eligibleRoles) {
-    log "  $(($eligibleRoles | Measure-Object).Count) eligible role(s) found"
-    log "  --------------------------"
-    $eligibleRoles | 
-        Format-Table -AutoSize PrincipalDisplayName,RoleDefinitionDisplayName,EndDateTime
+  function Enable-AzRoles {
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param (
+      $subscriptionName, 
+      $subscriptionId,
+      $Justification,
+      $Hours,
+      $rolesToIgnore
+    )
     
-    if($null -ne $activatedRoles) {
-      log "  $(($activatedRoles | Measure-Object).Count) activated role(s) found"
-      log "  ---------------------------"
-      $activatedRoles |
-          Format-Table -AutoSize PrincipalDisplayName,RoleDefinitionDisplayName,EndDateTime
+    begin {
+      log "Getting role information for $($subscriptionName) [$($subscriptionId)]."
+      
+      $filter = 'asTarget()'
+      $Scope = "/subscriptions/$subscriptionId"
     }
-    else {
-      log "No active role(s) found."
-    }
+    
+    process {
+      $eligibleRoles = Get-AzRoleEligibilitySchedule -Scope $scope -Filter $filter -ErrorAction stop |
+        # Where-Object { $_.Id -like "/subscriptions/$subscriptionId*" } |
+        Sort-Object PrincipalDisplayName, RoleDefinitionDisplayName
 
-    $expirationDuration = "PT{0}H" -f $hours #[XmlConvert]::ToString([TimeSpan]::FromHours($Hours))
-    [string]$roleExpireTime = $NotBefore.AddHours($Hours)
-    $userPrincipalId = (get-azaduser -UserPrincipalName (get-azcontext).account.Id).Id
+      if ($null -ne $eligibleRoles) {
+        log "  $(($eligibleRoles | Measure-Object).Count) eligible role(s) found"
+        log "  --------------------------"
+        $eligibleRoles | 
+          Sort-Object EndDateTime |
+          Format-Table -AutoSize PrincipalDisplayName, RoleDefinitionDisplayName, EndDateTime
+    
+        $activatedRoles = Get-AzRoleAssignmentScheduleInstance -Scope $scope -Filter $filter -ErrorAction stop |
+          Where-Object { $_.AssignmentType -EQ 'Activated' } |
+          # Where-Object { $_.Id -like "/subscriptions/$subscriptionId*" } |
+          Sort-Object PrincipalDisplayName, RoleDefinitionDisplayName
 
-    if (![System.String]::IsNullOrWhiteSpace($Justification)) {
-        $eligibleRoles |
-        Where-Object {$activatedRolesIds -notcontains $_.Id} |
-        Where-Object {$rolesToIgnore -notcontains $_.RoleDefinitionDisplayName} |
-        ForEach-Object {
-            $roleToActivate = $_
-            log "  Activating role:"
-            log "    ID:       " $roleToActivate.Id
-            log "    Principal:" $roleToActivate.PrincipalDisplayName
-            log "    Role:     " $roleToActivate.RoleDefinitionDisplayName
-            log "    Scope:    " $roleToActivate.Scope
-            $roleActivateParams = @{
+        $activatedRolesIds = $activatedRoles.LinkedRoleEligibilityScheduleId
+
+        if ($null -ne $activatedRoles) {
+          log "  $(($activatedRoles | Measure-Object).Count) activated role(s) found"
+          log "  ---------------------------"
+          $activatedRoles |
+            Sort-Object EndDateTime |
+            Format-Table -AutoSize PrincipalDisplayName, RoleDefinitionDisplayName, EndDateTime
+        }
+        else {
+          log "  No active role(s) found."
+        }
+
+        $expirationDuration = "PT{0}H" -f $hours #[XmlConvert]::ToString([TimeSpan]::FromHours($Hours))
+        [string]$roleExpireTime = $NotBefore.AddHours($Hours)
+        $userPrincipalId = (get-azaduser -UserPrincipalName (get-azcontext).account.Id).Id
+
+        if (![System.String]::IsNullOrWhiteSpace($Justification)) {
+          $newlyActivatedRoles = $eligibleRoles |
+            Where-Object { $activatedRolesIds -notcontains $_.Id } |
+            Where-Object { $rolesToIgnore -notcontains $_.RoleDefinitionDisplayName } |
+            ForEach-Object {
+              $roleToActivate = $_
+              log "  Activating role:"
+              log "    ID:       " $roleToActivate.Id
+              log "    Principal:" $roleToActivate.PrincipalDisplayName
+              log "    Role:     " $roleToActivate.RoleDefinitionDisplayName
+              log "    Scope:    " $roleToActivate.Scope
+              $roleActivateParams = @{
                 Name                            = New-Guid
                 Scope                           = $roleToActivate.Scope
                 PrincipalId                     = $userPrincipalId
@@ -101,19 +116,40 @@ process {
                 ExpirationType                  = 'AfterDuration'
                 ExpirationDuration              = $expirationDuration
                 Justification                   = $Justification
+              }
+                      
+              if ($PSCmdlet.ShouldProcess(
+                  "$($roleToActivate.RoleDefinitionDisplayName) on $($roleToActivate.ScopeDisplayName) ($($roleToActivate.ScopeId))",
+                  "Activate Role from $NotBefore to $roleExpireTime"
+                )) {
+                  New-AzRoleAssignmentScheduleRequest @roleActivateParams -ErrorAction Stop
+              }
             }
-                    
-            if ($PSCmdlet.ShouldProcess(
-                "$($roleToActivate.RoleDefinitionDisplayName) on $($roleToActivate.ScopeDisplayName) ($($roleToActivate.ScopeId))",
-                "Activate Role from $NotBefore to $roleExpireTime"
-            )) {
-                New-AzRoleAssignmentScheduleRequest @roleActivateParams -ErrorAction Stop
-            }
+          
+          $newlyActivatedRoles |
+            Sort-Object ExpirationDuration |
+            Format-Table -AutoSize PrincipalDisplayName, RoleDefinitionDisplayName, ExpirationDuration, ExpirationType
         }
+      }
+      else {
+        log "  No eligible role(s) found."
+      }      
+    }
+    end {
     }
   }
+
+  log "Starting"
+}
+process {
+  if ($subscriptionId) {
+    $subObject = Get-AzSubscription -SubscriptionId $subscriptionId
+    Enable-AzRoles -subscriptionName $subObject.Name  -subscriptionId $subObject.SubscriptionId -Justification $Justification -Hours $Hours -rolesToIgnore $rolesToIgnore
+  }
   else {
-    log "  No eligible role(s) found."
+    foreach ($subObject in $subscriptions) {
+      Enable-AzRoles -subscriptionName $subObject.Name  -subscriptionId $subObject.SubscriptionId -Justification $Justification -Hours $Hours -rolesToIgnore $rolesToIgnore
+    }
   }
 }
 end {
